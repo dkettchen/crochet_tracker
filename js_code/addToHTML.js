f6685c35241dec12c63f2b76543627376c3d9539
unknown <dkettchen@gmail.com>
2025-10-29 19:45:31 +0000

    refactor to scale to any number of rounds and add colour palettes to pick from

--- js_code/addToHTML.js
+++ js_code/addToHTML.js
@@ -1,9 +1,8 @@
 import { grannySquare } from "./grannySquarePattern.js";
 import { makeGrannySquareVector } from "./makeVectorGraphics.js";
 
-
 let pattern = grannySquare(3);
-let svg = makeGrannySquareVector(pattern);
+let svg = makeGrannySquareVector(7);
 
 let item = document.getElementById("svg_placeholder");
 
---
+++ js_code/colourPalettes.js
@@ -0,0 +1,10 @@
+// colour palettes to use
+export const FALL = [
+    "sienna", "red", "darkorange", "chocolate", "orangered", "firebrick", "orange",
+];
+export const OCEAN = [
+    "turquoise", "seagreen", "navy", "darkcyan", "deepskyblue", "lightseagreen", "midnightblue",
+];
+export const RAINBOW = [
+    "blueviolet", "navy", "dodgerblue", "green", "goldenrod", "darkorange", "crimson",
+];
